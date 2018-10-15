da9803af3b53fbb95bdd1f2758d953b783193dd2
Alston Kim <sahyun1@hotmail.com>
2018-10-15 16:54:03 +1300

    node-schedule to run at specific time

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var AWS = require('aws-sdk');
 const express = require('express');
 const app = express();
+const port= 3000;
 const request = require('request');
 const moment = require('moment');
+var schedule = require('node-schedule');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+let utcTime = moment.utc().hour(3).minutes(0);
+let localHour = utcTime.local().hour();
+
+console.log(localHour);
+// let repeatAt ={hour: 11, minute: 25, tz:'Asia/Seoul'};
+// let repeatAt ={hour: 15, minute: 39, tz:'Pacific/Auckland'};
+// let repeatAt ={hour: 16, minute: 15, tz:'Europe/London'};
+let repeatAt ={hour: localHour, minute: 37};
+
+
+// var j = schedule.scheduleJob('21 1 * * *','Europe/London', function(){
+var j = schedule.scheduleJob(repeatAt, function(){
+    request('https://api.coingecko.com/api/v3/exchanges/bithumb', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            generateCSV(JSON.parse(body));
+            // res.send("done");
+        }
+    });
+});
 
 app.get('/generate',(req,res)=>{
 
@@ -22,10 +44,11 @@ app.get('/generate',(req,res)=>{
 });
 
 function generateCSV(returnValue){
-    let momentTime = moment.utc().format('YYYYMMDDhhmm');
-
+    let momentTime = moment.utc().format('YYYYMMDDHHmm');
+    // let momentTime = moment().format('YYYYMMDDHHmm');
+    let exchangeName = returnValue.name;
     const csvWriter = createCsvWriter({
-        path: './tradingVolume_'+momentTime+'.csv',
+        path: './'+exchangeName+'_'+momentTime+'.csv',
         header: [
             {id: 'base', title: 'Base'},
             {id: 'target', title: 'Target'},
@@ -34,8 +57,9 @@ function generateCSV(returnValue){
     });
 
     const records = [];
+    records.push({volume:returnValue.trade_volume_24h_btc});
     for(let record of returnValue.tickers){
-        records.push({base:record.base, target:record.target, volume:record.volume});
+        records.push({base:record.base, target:record.target, volume:record.converted_volume.usd});
     }
 
     csvWriter.writeRecords(records)       // returns a promise
